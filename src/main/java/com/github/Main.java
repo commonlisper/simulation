21d18720a7a6e9d34c9b6484c345e5b0e3aae172
Common Lisper <atosdo@live.com>
2025-01-15 17:50:11 +0200

    initial project setup

---
+++ src/main/java/com/github/Main.java
@@ -0,0 +1,10 @@
+package com.github;
+
+import com.github.application.Simulation;
+
+public class Main {
+    public static void main(String[] args) {
+        Simulation simulation = new Simulation();
+        simulation.start();
+    }
+}
---
+++ src/main/java/com/github/application/Simulation.java
@@ -0,0 +1,11 @@
+package com.github.application;
+
+public class Simulation {
+    public void start() {
+        System.out.println("Simulation starts!");
+    }
+
+    public void stop() {
+
+    }
+}
